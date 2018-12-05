3dda2e1824db0308d3f052bb3787e544261f2a12
jennygrahamjones <jenny.grahamjones@gmail.com>
2018-12-05 09:26:31 +0000

    Dependency updates

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -11,13 +11,13 @@ object MicroServiceBuild extends Build with MicroService {
   val compile = Seq(
     "uk.gov.hmrc" %% "play-reactivemongo" % "6.2.0",
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.0.0",
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.2.0",
     "uk.gov.hmrc" %% "domain" % "5.2.0",
     "com.eclipsesource" %% "play-json-schema-validator" % "0.8.9"
   )
 
   def test(scope: String = "test,it") = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % "3.2.0" % scope,
+    "uk.gov.hmrc" %% "hmrctest" % "3.3.0" % scope,
     "org.scalatest" %% "scalatest" % "3.0.0" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
