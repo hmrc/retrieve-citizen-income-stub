2939a80a5815f3290fd59d87fef3131826acfe0e
nik <ward.nickjames@gmail.com>
2022-08-22 16:18:13 +0100

    Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,22 +5,23 @@ import sbt._
 
 object AppDependencies {
 
+  val bootstrapPlayVersion = "7.0.0"
+
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"                       %% "bootstrap-backend-play-28" % "5.12.0",
+    "uk.gov.hmrc"                       %% "bootstrap-backend-play-28" % bootstrapPlayVersion,
     "uk.gov.hmrc"                       %% "domain"                    % "6.2.0-play-28",
     "com.github.everit-org.json-schema" %  "org.everit.json.schema"    % "1.13.0"
   )
 
   val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapPlayVersion,
     "org.scalatest"          %% "scalatest"          % "3.2.9",
     "org.pegdown"             % "pegdown"            % "1.6.0",
-    "org.mockito"             % "mockito-core"       % "3.12.4",
+    "org.mockito"             % "mockito-core"       % "4.7.0",
     "org.scalacheck"         %% "scalacheck"         % "1.15.4",
     "org.scalatestplus" %% "scalacheck-1-14" % "3.1.1.1",
-    "com.typesafe.play"      %% "play-test"          % PlayVersion.current,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0",
-    "com.vladsch.flexmark" % "flexmark-all" % "0.35.10"
+    "com.typesafe.play"      %% "play-test"          % PlayVersion.current
   ).map(_ % "test")
 
   val all: Seq[ModuleID] = compile ++ test
