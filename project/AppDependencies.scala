e7d699c92f5e329b711eedc094d2624dacf49638
Ian Godman <IanPGodman@users.noreply.github.com>
2023-07-18 07:58:49 +0100

    Think problem with signing commits resolved

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,22 +5,23 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapPlayVersion = "7.12.0"
+  private val bootstrapPlayVersion = "7.19.0"
+  private val playVersion = "-play-28"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"                       %% "bootstrap-backend-play-28" % bootstrapPlayVersion,
-    "uk.gov.hmrc"                       %% "domain"                    % "8.1.0-play-28",
-    "com.github.everit-org.json-schema" %  "org.everit.json.schema"    % "1.13.0"
+    "uk.gov.hmrc"                       %% s"bootstrap-backend$playVersion" % bootstrapPlayVersion,
+    "uk.gov.hmrc"                       %% "domain"                         % s"8.3.0$playVersion",
+    "com.github.everit-org.json-schema" %  "org.everit.json.schema"         % "1.13.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-28" % bootstrapPlayVersion,
-    "org.pegdown"       % "pegdown"                 % "1.6.0",
-    "org.mockito"       % "mockito-core"            % "4.7.0",
-    "org.scalacheck"    %% "scalacheck"             % "1.16.0",
-    "org.scalatestplus" %% "scalacheck-1-14"        % "3.2.2.0",
-    "com.typesafe.play" %% "play-test"              % PlayVersion.current
+    "uk.gov.hmrc"       %% s"bootstrap-test$playVersion" % bootstrapPlayVersion,
+    "org.pegdown"       % "pegdown"                      % "1.6.0",
+    "org.mockito"       % "mockito-core"                 % "4.11.0",
+    "org.scalacheck"    %% "scalacheck"                  % "1.16.0",
+    "org.scalatestplus" %% "scalacheck-1-14"             % "3.2.2.0",
+    "com.typesafe.play" %% "play-test"                   % "2.8.19"
   ).map(_ % "test")
 
   val all: Seq[ModuleID] = compile ++ test
